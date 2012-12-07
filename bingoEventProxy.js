7341c0c3d66506f549ef672864ec1c7bf16ffb0e
bingowarden <34237598@qq.com>
2012-12-07 09:12:22 +0800

    first version

---
+++ bingoEventProxy.js
@@ -0,0 +1,34 @@
+function eventProxy(){
+    this.evs = {};
+    this.finalargs = [];
+}
+eventProxy.prototype.todo = function(){
+    var args = [].slice.call(arguments);
+    this.finalCallBack = args[args.length-1];
+    for(var i=0;i<args.length-1;i++){
+        this.evs[args[i]] = {
+            triggered:false,
+            index:i
+        }
+    }
+}
+eventProxy.prototype.destory = function(){
+    this.evs = {};
+    this.finalargs = [];
+	delete this.finalCallBack;
+}
+eventProxy.prototype.trigger = function(eventname,data){
+    var self = this;
+    this.evs[eventname].triggered = true;
+    var alldone = true,finalargs = this.finalargs;
+    finalargs[this.evs[eventname].index] = data;
+    for(var p in this.evs){
+        if(!this.evs[p].triggered){
+            alldone = false;
+			break;
+        }
+    }
+   alldone && this.finalCallBack.apply(null,finalargs);
+}
+
+module.exports = eventProxy;
---
+++ example.js
@@ -0,0 +1,19 @@
+var eventProxy = require('bingoEvent');
+
+var ep = new eventProxy();
+
+function finalCallBack(v1data,v2data,v3data){
+	console.log(v1data,v2data,v3data);
+}
+
+ep.todo('v1','v2','v3',finalCallBack);
+
+setTimeout(function(){
+	ep.trigger('v1',{v1data:'v1data'})
+},5000);
+setTimeout(function(){
+	ep.trigger('v2','v2data');
+},2000);
+setTimeout(function(){
+	ep.trigger('v3',['v3data'])
+},3000);
